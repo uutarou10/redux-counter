a6ebbd82ce7eb5a694237cadb7dcecc4acdb4bd3
Kota Nonaka <uutarou@innovation.local>
2017-06-05 15:12:40 +0900

    Initialize commit

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <div>
+      <p>Counter: </p>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default Counter;
+
